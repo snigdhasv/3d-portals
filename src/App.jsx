48109b00c978d5e70fefe1a88c93f66c5e84e02d
snigdhasv <snigdhasv1987@gmail.com>
2024-06-05 11:45:45 +0530

    boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,20 @@
-import React, { useEffect, useRef } from "react";
-import { Canvas } from "@react-three/fiber";
-import { useLoader } from '@react-three/fiber';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { OrbitControls, Stats } from "@react-three/drei";
-import "./App.css";
+import { Canvas } from '@react-three/fiber'
+import './App.css'
+import { OrbitControls } from '@react-three/drei'
 
-function printMeshHierarchy(node, prefix = '') {
-  if (!node) return;
-  console.log(`${prefix}${node.name} (${node.type})`);
-  if (node.children && node.children.length > 0) {
-    node.children.forEach(child => printMeshHierarchy(child, `${prefix}  `));
-  }
-}
-
-function Scene() {
-  const gltf = useLoader(GLTFLoader, '/jewel_rio_fbx.glb');
-  const modelRef = useRef();
-
-  useEffect(() => {
-    if (modelRef.current) {
-      console.log('Model loaded and reference set');
-      printMeshHierarchy(modelRef.current);
-    } else {
-      console.log('Model reference is not set yet');
-    }
-  }, [gltf]);
-
-  return <primitive object={gltf.scene} ref={modelRef} />;
-}
 
 export default function App() {
   return (
-    <Canvas style={{ background: "#171717" }}>
-      <OrbitControls />
-      <ambientLight />
-      <Scene />
-      <Stats />
-    </Canvas>
-  );
+    <>
+      <Canvas camera={{ position: [-8, 5, 8] }}>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <mesh scale={3}>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh>
+        <OrbitControls />
+      </Canvas>
+    </>
+  )
 }
